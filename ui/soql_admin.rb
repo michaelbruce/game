8178552b1a4edd41b078a647f9428041c7610429
Michael Bruce <michael.bruce@singletracksystems.com>
2015-03-04 11:25:27 +0000

    cleared up soql admin ui, using text area for entry panel now with default text

--- ui/soql_admin.rb
+++ ui/soql_admin.rb
@@ -5,6 +5,7 @@ import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.JButton
 import javax.swing.JTextField
+import javax.swing.JTextArea
 import javax.swing.JTable
 import javax.swing.JSplitPane
 import java.awt.FlowLayout
@@ -17,51 +18,14 @@ class MainWindow < JFrame
     super "soqlAdmin"
     setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
 
-#    #these two vars aren't java object[]s
-#    headers = ["Id","Name","Email","OwnerId"].to_java(:object)
-#    data = [
-#      [001, "Johnny", "johnny@walker.com", "A5dre0000did"].to_java,
-#      [002, "Johnny", "johnny@walker.com", "A5dre0000did"].to_java,
-#      [003, "Johnny", "johnny@walker.com", "A5dre0000did"].to_java,
-#      [004, "Johnny", "johnny@walker.com", "A5dre0000did"].to_java
-#    ].to_java
-#    JTable table = JTable.new(data, headers)
-
-    data_panel = JPanel.new(FlowLayout.new(FlowLayout::CENTER))
-    #panel = JPanel.new(FlowLayout.new(FlowLayout::CENTER))
-    #input = JTextField.new(10)
-    #button = JButton.new("Encrypt")
-    #output = JTextField.new(10)
-
-    #panel.add input
-    #panel.add button
-    #panel.add output
-
-    #data_panel.add table
-    test_table = javax.swing.table.DefaultTableModel.new
-    test_table.add_column("Id")
-    test_table.add_column("Name")
-    test_table.add_column("Email")
-    test_table.add_column("OwnerId")
-    test_table.add_row(["1","Johnny","johnny@walker.com","9A40040403IA"].to_java)
-    test_table.add_row(["2","Johnny","johnny@walker.com","9A40040403IA"].to_java)
-    test_table.add_row(["3","Johnny","johnny@walker.com","9A40040403IA"].to_java)
-    test_jtab = JTable.new(test_table)
-    data_panel.add test_jtab
-
-    #button.add_action_listener do|e|
-    #  output.text = input.text.upcase
-    #end
-
     split = JSplitPane.new(JSplitPane::VERTICAL_SPLIT)
 
-    #add panel
-    #setSize 300, 300
     entry_panel = create_entry_panel
     split.setTopComponent(entry_panel)
     setSize 300, 300
     setVisible true
 
+    data_panel = create_data_panel
     split.setBottomComponent(data_panel)
     setLocation 0, 300
     setSize 300, 300
@@ -72,9 +36,26 @@ class MainWindow < JFrame
     setVisible true
   end
 
+  def create_data_panel
+    data_panel = JPanel.new(FlowLayout.new(FlowLayout::CENTER))
+    test_table = javax.swing.table.DefaultTableModel.new
+    test_table.add_column("Id")
+    test_table.add_column("Name")
+    test_table.add_column("Email")
+    test_table.add_column("OwnerId")
+    test_table.add_row(["1","Johnny","johnny@walker.com","9A40040403IA"].to_java)
+    test_table.add_row(["2","Johnny","johnny@walker.com","9A40040403IA"].to_java)
+    test_table.add_row(["3","Johnny","johnny@walker.com","9A40040403IA"].to_java)
+    test_jtab = JTable.new(test_table)
+    data_panel.add test_jtab
+    data_panel
+  end
+
   def create_entry_panel
     panel = JPanel.new(FlowLayout.new(FlowLayout::CENTER))
-    input = JTextField.new(10)
+    input = JTextArea.new(3,20)
+    input.setEditable true
+    input.setText 'select * from Contacts'
     panel.add input
     panel
   end
@@ -82,3 +63,13 @@ end
 
 MainWindow.new
 
+#spare jruby/swing example code
+    #panel.add input
+    #panel.add button
+    #panel.add output
+
+    #data_panel.add table
+
+    #button.add_action_listener do|e|
+    #  output.text = input.text.upcase
+    #end
