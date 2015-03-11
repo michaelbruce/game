fab360faeba40fc5ff7567635ea76502e146588e
michaelbruce <michael.bruce@singletracksystems.com>
2015-03-11 08:28:45 +0000

    Added to ruby chess files, now creates all pieces on class init

--- chess/chess.rb
+++ chess/chess.rb
@@ -1,4 +1,37 @@
+require './piece'
+
 class Chess
+  attr_reader :pieces
+
+  def initialize
+    @pieces = setup_board
+  end
+
+  def setup_board
+    pieces = []
+    #Creating dark side
+    pieces << Knight.new('dark',2,1) #B1
+    pieces << Knight.new('dark',7,1) #G1
+    pieces << Rook.new('dark',1,1) #A1
+    pieces << Rook.new('dark',8,1) #H1
+    pieces << Bishop.new('dark',3,1) #C1
+    pieces << Bishop.new('dark',6,1) #F1
+    pieces << Queen.new('dark',4,1) #D1
+    pieces << King.new('dark',5,1) #E1
+    (1..8).each {|num| pieces << Pawn.new('dark', num, 2)} #A2-H2
+
+    #Creating light side
+    pieces << Knight.new('light',2,8) #B8
+    pieces << Knight.new('light',7,8) #G8
+    pieces << Rook.new('light',1,8) #A8
+    pieces << Rook.new('light',8,8) #H8
+    pieces << Bishop.new('light',3,8) #C8
+    pieces << Bishop.new('light',6,8) #F8
+    pieces << Queen.new('light',4,8) #D8
+    pieces << King.new('light',5,8) #E8
+    (1..8).each {|num| pieces << Pawn.new('light', num, 7)} #A7-H7
+    pieces
+  end
 end
 
 class Board
@@ -17,3 +50,7 @@ class Board
   end
 
 end
+
+tc = Chess.new
+p tc.pieces
+p "total: #{tc.pieces.size}"
--- chess/piece.rb
+++ chess/piece.rb
@@ -13,27 +13,77 @@ class Piece
 
 
   def forward
-    raise OffBoardMove, "this move would take the piece of the board" if @column = 8
+    raise OffBoardMove, "this move would take the piece of the board" if @column == 8
     @column += 1
   end
 
   def backward
-    raise OffBoardMove, "this move would take the piece of the board" if @column = 1
+    raise OffBoardMove, "this move would take the piece of the board" if @column == 1
     @column -= 1
   end
 
   def left
-    raise OffBoardMove, "this move would take the piece of the board" if @row = 1
+    raise OffBoardMove, "this move would take the piece of the board" if @row == 1
     @row -= 1
   end
 
   def right
-    raise OffBoardMove, "this move would take the piece of the board" if @row = 8
+    raise OffBoardMove, "this move would take the piece of the board" if @row == 8
     @row += 1
   end
 end
 
 class Knight < Piece
+  def position
+    ["A5","B6"]
+  end
+
+  def jump
+    [3,2] # or use Piece methods?
+  end
+end
+
+class Rook < Piece
+  def position
+    ["A5","B6"]
+  end
+
+  def jump
+    [3,2] # or use Piece methods?
+  end
+end
+
+class Bishop < Piece
+  def position
+    ["A5","B6"]
+  end
+
+  def jump
+    [3,2] # or use Piece methods?
+  end
+end
+
+class Queen < Piece
+  def position
+    ["A5","B6"]
+  end
+
+  def jump
+    [3,2] # or use Piece methods?
+  end
+end
+
+class King < Piece
+  def position
+    ["A5","B6"]
+  end
+
+  def jump
+    [3,2] # or use Piece methods?
+  end
+end
+
+class Pawn < Piece
   def position
     ["A5","B6"]
   end
