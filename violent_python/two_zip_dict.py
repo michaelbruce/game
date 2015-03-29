f0c743fb8d697d69a0f19e5d4758e732a68bffdb
Michael Bruce <michael.bruce@singletrack.com>
2015-03-29 15:22:42 +0100

    Added pwd zip and example two before threading

--- violent_python/two_zip_dict.py
+++ violent_python/two_zip_dict.py
@@ -1,6 +1,33 @@
 import zipfile
 
-#need to create a zipfile too...
-zFile = zipfile.ZipFile("evil.zip")
-try:
-    zFile.extractall(pwd="secret")
+def extract_file(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        return password
+    except:
+        return
+
+def main():
+    zFile = zipfile.ZipFile('file.zip')
+    passFile = open('dictionary.txt')
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extract_file(zFile, password)
+        if guess:
+            print '[+] Password = ' + password + '\n'
+            exit(0)
+
+if __name__ == '__main__':
+    main()
+
+# zFile = zipfile.ZipFile("file.zip")
+# passFile = open('dictionary.txt')
+#
+# for line in passFile.readlines():
+#     password = line.strip('\n')
+# try:
+#     zFile.extractall(pwd=password)
+#     print '[+} Password = ' + password + '\n'
+#     exit(0)
+# except Exception, e:
+#     pass
