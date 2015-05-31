902d4e2e86d5faf155d803cd936d273d3b2c7733
Michael Bruce <michael.bruce@singletrack.com>
2015-05-31 17:10:29 +0100

    Added Euler problem 2

--- euler.rb
+++ euler.rb
@@ -6,6 +6,15 @@ def multiples_of_three_and_five
   multiples_list.inject{|sum, number| sum += number}
 end
 
+def even_fibonacci_numbers
+  fibonacci_sequence = [1]
+  while fibonacci_sequence[-1] < 4000000
+    fibonacci_sequence << fibonacci_sequence[-1] + (fibonacci_sequence[-2] || fibonacci_sequence[-1])
+  end
+  fibonacci_sequence.keep_if{|num| num % 2 == 0}
+  fibonacci_sequence.inject{|sum,number| sum += number}
+end
 
 
 puts "Problem 1: #{multiples_of_three_and_five}"
+puts "Problem 2: #{even_fibonacci_numbers}"
