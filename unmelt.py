5b0a2d589c85b0c80ba9af62d3cb5c271d22a691
Michael Bruce <michael.bruce@singletrack.com>
2015-05-31 22:15:31 +0100

    Added melting file

---
+++ unmelt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print('Starting script')
+
+# aim - convert list of generic actions that contain information on types (call, meeting, support)
+# return entries with list data sorted into columns of these different types.
+
